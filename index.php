8514285d518c4a8ae60bead00bcb1d4f741f0c33
Lukáš Nekola <nekola.luky@seznam.cz>
2023-06-02 16:40:09 +0200

    working_app

--- index.php
+++ index.php
@@ -75,15 +75,15 @@
                         </select>              
                     </div>
                     <div id="checkFirst" class="col-lg">
-                        <input class="form-check-input" type="radio" name="newCheck" id="flexRadioDefault" checked> 
+                        <input class="form-check-input" value="withoutLanduse" type="radio" name="LLUC" id="flexRadioDefault" checked> 
                         <label class="form-check-label" for="flexRadioDefault">nezobrazovat využití území</label>
                     </div>       
                     <div id="checkSecond" class="col-lg"> 
-                        <input class="form-check-input" type="radio" name="newCheck" id="flexRadioDefault2">    
+                        <input class="form-check-input" value="oldLanduse" type="radio" name="LLUC" id="flexRadioDefault2">    
                         <label class="form-check-label" for="flexRadioDefault2">zobrazit využití území 19. století</label>  
                     </div>
                     <div id="checkThird" class="col-lg">
-                        <input class="form-check-input" type="radio" name="newCheck" id="flexRadioDefault3">  
+                        <input class="form-check-input" value="newLanduse" type="radio" name="LLUC" id="flexRadioDefault3">  
                         <label class="form-check-label" for="flexRadioDefault3">zobrazit využití území 2019</label>
                     </div>           
                 <div id="button">
@@ -106,22 +106,6 @@
     </div>
 </div> 
 
-<!--
-  <div id='frame'>
-    <div class='search'>
-      <form method='post'>
-        <select id="selectBox" class="selectBox" required>
-          <option value="">--Select--</option>
-          <option value="Yes">Yes</option>
-          <option value="No">No</option>
-        </select>
-        <input type='text' name='searchQuery' id='searchInput' required/>
-        <input type='submit' name='submit' id='submitBtn' class='enableOnInput' disabled='disabled' />
-      </form>
-    </div>
-  </div>
-    -->
-
 <script>
     var removed;
     $('#selectFirst').change( function() {
@@ -138,11 +122,11 @@
 </script>
 
     <?php
-
-       if(isset($_POST["firstAlt"]) && isset($_POST["secondAlt"]) && isset($_POST["diffAlt"]) && isset($_POST["operAlt"]) && isset($_POST["upDown"])){ //both values must be filled.
+        //druhé testování na straně backendu na úplnost formuláře
+        if(isset($_POST["firstAlt"]) && isset($_POST["secondAlt"]) && isset($_POST["diffAlt"]) && isset($_POST["operAlt"]) && isset($_POST["upDown"])){ //both values must be filled.
             $firstYear = $_POST["firstAlt"];
             $secondYear = $_POST["secondAlt"];
-
+            //přetypování proměnné prvního roku z čísla na string
             if($firstYear == 1) {
                 $firstYear = 'altitude_1938';
             }
@@ -152,7 +136,8 @@
             elseif($firstYear == 3) {
                 $firstYear = 'altitude_2000';
             }
-    
+            
+            //přetypování proměnné druhého roku z čísla na string
             if($secondYear == 2) {
                 $secondYear = 'altitude_1951';
             }
@@ -163,92 +148,100 @@
                 $secondYear = 'altitude_2020';
             }
 
+            //přeno zbylých proměnných
             $diff = $_POST["diffAlt"];
             $oper = $_POST["operAlt"];
             $plusMinus = $_POST["upDown"];
+            $landUse = $_POST['LLUC'];
 
-            //number of row based on form
-            $queryNumberRow = "SELECT * FROM altitudes_holesice where ($firstYear - $secondYear) $oper $diff";
-            $resultNumberRow = pg_query($queryNumberRow);    
-            $rowsNumber = pg_num_rows($resultNumberRow);
-            echo $rowsNumber;
             
             
             $queryDrop = "DROP TABLE IF EXISTS show_polygon_altitude";
             $resultOne= pg_query($queryDrop);
             
+            //
             if(($oper == '<' && $plusMinus == 'plus') || ($oper == '<' && $plusMinus == 'minus')) {
+                //
                 if($plusMinus == 'minus'){
-                    $diff = -($diff); //funguje
+                    $diff = -($diff); 
+                    $rulesQuery = "($secondYear - $firstYear) between $diff and 0";
                     echo "menší pokles";
-                    $query = "CREATE TABLE show_polygon_altitude as(
-                        SELECT
-                            ST_NumGeometries(cluster_make),
-                            st_transform(st_concavehull(cluster_make, 0.005, true),4326) AS final_collection
-                                FROM (SELECT unnest(ST_ClusterWithin(st_transform(geom, 32633), 16)) as cluster_make
-                                    FROM (SELECT * 
-                                        FROM altitudes_holesice as table_main
-                                            WHERE (($secondYear - $firstYear) between $diff and 0)) as main_query
-                                ) as cluster_main_query
-                                    WHERE st_numgeometries(cluster_make) > 20)";
                 }
+                //
                 elseif($plusMinus == 'plus'){
+                    $rulesQuery = "($secondYear - $firstYear) between 0 and $diff";
                     echo "menší navýšení";
-                    $query = "CREATE TABLE show_polygon_altitude as(
-                        SELECT
-                            ST_NumGeometries(cluster_make),
-                            st_transform(st_concavehull(cluster_make, 0.005, true),4326) AS final_collection
-                                FROM (SELECT unnest(ST_ClusterWithin(st_transform(geom, 32633), 5)) as cluster_make
-                                    FROM (SELECT * 
-                                        FROM altitudes_holesice as table_main
-                                            WHERE (($firstYear - $secondYear) between 0 and $diff)) as main_query
-                                ) as cluster_main_query
-                                    WHERE st_numgeometries(cluster_make) > 20)";
                 }
-                
             }
             else { 
-                if($plusMinus == 'plus') { //fajn skript a funguje
-                    echo "větší navýšení než 10";
-                    $query = "CREATE TABLE show_polygon_altitude as(
-                        SELECT
-                            ST_NumGeometries(cluster_make),
-                            st_transform(st_concavehull(cluster_make, 0.005, true),4326) AS final_collection
-                                FROM (SELECT unnest(ST_ClusterWithin(st_transform(geom, 32633), 16)) as cluster_make
-                                    FROM (SELECT * 
-                                        FROM altitudes_holesice as table_main
-                                            WHERE (($secondYear - $firstYear) > $diff)) as main_query
-                                ) as cluster_main_query
-                                    WHERE st_numgeometries(cluster_make) > 20)";
+                //zobrazit kladné změny výšky větší, nežli je prahová hodnota
+                if($plusMinus == 'plus') { 
+                    $rulesQuery = "($secondYear - $firstYear) > $diff";
                 }
-                elseif($plusMinus == 'minus') {  // fajn skript a funguje
-                    echo "větší pokles než 10";
-                    $query = "CREATE TABLE show_polygon_altitude as(
-                        SELECT
-                            ST_NumGeometries(cluster_make),
-                            st_transform(st_concavehull(cluster_make, 0.005, true),4326) AS final_collection
-                                FROM (SELECT unnest(ST_ClusterWithin(st_transform(geom, 32633), 16)) as cluster_make
-                                    FROM (SELECT * 
-                                        FROM altitudes_holesice as table_main
-                                            WHERE (($firstYear - $secondYear) > $diff)) as main_query
-                                ) as cluster_main_query
-                                    WHERE st_numgeometries(cluster_make) > 20)";
+                //zobrazit záporné změny výšky větší, nežli je prahová hodnota
+                elseif($plusMinus == 'minus') {  
+                    $rulesQuery = "($firstYear - $secondYear) > $diff";
+                    
                 }   
             }
+
+            //testování existence vyselektovaných dat a prvotní
+            $queryNumberRow = "SELECT * FROM altitudes_holesice where $rulesQuery";
+            $resultNumberRow = pg_query($queryNumberRow);    
             
+            if(pg_num_rows($resultNumberRow) == 0) {
+                exit("Žádná existující data");
             }
+            /*
+            if() {
+
+            }
+            */
+
+            $mainQuery = "CREATE TABLE show_polygon_altitude as(
+                SELECT
+                    ST_NumGeometries(cluster_make),
+                    st_transform(st_concavehull(cluster_make, 0.005, true),4326) AS final_collection
+                        FROM (SELECT unnest(ST_ClusterWithin(st_transform(geom, 32633), 16)) as cluster_make
+                            FROM (SELECT * 
+                                FROM altitudes_holesice as table_main
+                                    WHERE ($rulesQuery)) as main_query
+                        ) as cluster_main_query
+                            WHERE st_numgeometries(cluster_make) > 20)";
+            
+            pg_query($mainQuery);
+
+            if($landUse == 'withoutLanduse'){
+                
+                $cutPolygonQuery = "UPDATE show_polygon_altitude as b
+                                    SET final_collection = ST_Intersection(b.final_collection, p.geom)
+                                        FROM cadastral_pol as p
+                                            WHERE ST_Intersects(b.final_collection, p.geom)";
+  
+            }
+            else {
+                if($landUse == 'oldLanduse') {
+                    //proměnné
+                }
+                elseif($landUse == 'newLanduse') {
+                    //proměnné
+                }
+
+                //skript pro layout
+            }
+            
+        }
         else{
             echo 'Je třeba vyplnit všechna pole';
             pg_close($db_connect);
         }
-        
-        $result = pg_query($query);
-        //$result = pg_query($query) or die('Error message: ' . pg_last_error());
-        /*
+        $result = pg_query($cutPolygonQuery) or die('Error message: ' . pg_last_error());
+
         while ($row = pg_fetch_row($result)) {
-            var_dump($row);
+            echo var_dump($row);
+            
         }
-        */
+        
         //pg_free_result($result);
         pg_close($db_connect);
                 
